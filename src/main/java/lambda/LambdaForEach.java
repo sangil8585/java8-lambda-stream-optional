f0604151697c98fa731d0dc33fcf72b017a31dfa
Sangil Byun <sangil8585@naver.com>
2022-07-06 00:40:15 +0900

    lambda - 람다식을 이용한 List forEach문 예제

--- src/main/java/lambda/LambdaForEach.java
+++ src/main/java/lambda/LambdaForEach.java
@@ -12,8 +12,8 @@ public class LambdaForEach {
          * collection에는 배열, 리스트 등의 데이터를 저장하는 변수명을 지정합니다.
          * forEach 함수는 람다식을 작성해야 하지만, 간단한 반복 처리같은 경우에는 쉽게 이해가 가능하다.
          *
-         * for문과 forEach문에서 출력한 결과 값은 같다.
-         * forEach문은 람다식으로 표현하기 때문에 소스도 간결하게 작성할 수 있다.
+         * for문과 forEach에서 출력한 결과 값은 같다.
+         * forEach은 람다식으로 표현하기 때문에 소스도 간결하게 작성할 수 있다.
          */
         List<String> list = new ArrayList();
         list.add("byun");
@@ -31,7 +31,7 @@ public class LambdaForEach {
         /**
          * author : byun sangil
          * date : 2022-07-04
-         * description : 배열에서 forEach문 사용방법
+         * description : 배열에서 forEach 사용방법
          * 배열에서 forEach 함수를 사용하기 위해서는 Stream API를 이용해야한다.
          * 배열로 값을 저장해한 arr 변수를 Arrays.stream() 안에 지정해서 forEach 함수로 출력하고 있다.
          *
@@ -44,7 +44,7 @@ public class LambdaForEach {
         /**
          * author : byun sangil
          * date : 2022-07-05
-         * description : MAP forEach문 사용방법
+         * description : MAP forEach 사용방법
          * MAP은 KEY와 VALUE값으로 구성되어 있다. forEach를 사용하여 MAP에 들어있는 값을
          * 쉽게 출력할 수 있다.
          */
@@ -55,7 +55,22 @@ public class LambdaForEach {
         map.put("BRUCE", "HULK");
 
         map.forEach((key, value) -> System.out.println(key + " : " + value));
-        //
+
+
+        /**
+         * author : byun sangil
+         * date : 2022-07-06
+         * description : List forEach 사용방법
+         * indexOf 함수를 사용하여 인덱스 번호로 List에서 요소를 취득하여 출력함.
+         * List값을 출력할 때는 lambda식을 생략하여 작성할 수 있다.
+         */
+        List<String> list2 = new ArrayList<>();
+        list2.add("byun");
+        list2.add("sang");
+        list2.add("il");
+
+        list2.forEach(s -> System.out.println(list2.indexOf(s) + " : " + s));
+        list2.forEach(System.out::println); //람다식 생략 예제
     }
 
 }
